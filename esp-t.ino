981e2104b5e0a94282d205a7b87b38c919f8b31c
TryIoT <34625592+TryIoT@users.noreply.github.com>
2017-12-27 23:07:00 +0100

    setup the source file and added the flashing onboard LED

--- esp-t.ino
+++ esp-t.ino
@@ -1,9 +1,50 @@
-void setup() {
-  // put your setup code here, to run once:
+//******************************************************************************
+//
+// esp-t
+//
+// Learning project for ESP32 on ARDUINO IDE
+//
+// Read DS18B20 sensors, save and send the data to a remote target using MQTT
+//
+//******************************************************************************
 
+//------------------------------------------------------------------------------
+// Variables
+
+int LED_BUILTIN = 2;
+
+// End Variables
+//------------------------------------------------------------------------------
+
+// Flash the LED at led_pin with a led_delay led_count times.
+void LedFlash(int led_pin, int led_delay, int led_count)
+{
+  for (int i = 0; i < led_count; ++i)
+  {
+    digitalWrite(led_pin, HIGH);
+    delay(led_delay);
+    digitalWrite(led_pin, LOW);
+    delay(led_delay);
+  }
 }
 
+
+
+// Arduino setup
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Flash LED threetimes at end of setup
+  LedFlash(LED_BUILTIN, 100, 3);
+}
+
+// Arduino loop
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // Flash LED once:
+  LedFlash(LED_BUILTIN, 500, 1);
 }
+
+// END
